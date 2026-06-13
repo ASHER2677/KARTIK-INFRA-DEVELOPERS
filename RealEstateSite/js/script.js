60f83070e74b77eedf2695c70322a0e8d4deade0
ASHER2677 <asherjohn.asher26@gmail.com>
2026-06-13 11:04:27 +0530

    Add files via upload

---
+++ RealEstateSite/js/script.js
@@ -0,0 +1,13 @@
+window.addEventListener('scroll', function(){
+
+    const nav = document.querySelector('nav');
+
+    if(window.scrollY > 50){
+        nav.style.boxShadow =
+        '0 5px 20px rgba(0,0,0,0.1)';
+    }
+    else{
+        nav.style.boxShadow = 'none';
+    }
+
+});
